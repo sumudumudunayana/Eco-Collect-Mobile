eb8369873c80ee931a40ff12be105f0df87a8ee8
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-08-10 23:53:15 +0530

    safe area added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import AppNavigator from './android/app/src/navigation/AppNavigator';
 
 function App() {
-  return <AppNavigator />;
+  return (
+    <SafeAreaProvider>
+
+      <StatusBar
+        backgroundColor="#2E7D32"
+        barStyle="light-content"
+      />
+
+      <AppNavigator />
+
+    </SafeAreaProvider>
+  );
 }
 
 export default App;
