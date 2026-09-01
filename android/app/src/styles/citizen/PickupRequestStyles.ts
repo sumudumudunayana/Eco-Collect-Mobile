0d7c1f048afa29b7b21eb60d64a51d0990c5f105
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-02 00:46:24 +0530

    pickup request styles updated

---
+++ android/app/src/styles/citizen/PickupRequestStyles.ts
@@ -0,0 +1,58 @@
+import {StyleSheet} from 'react-native';
+
+
+export default StyleSheet.create({
+
+container:{
+  flex:1,
+  backgroundColor:'#F5F9F5'
+},
+
+
+form:{
+  padding:20
+},
+
+
+label:{
+  fontSize:15,
+  fontWeight:'600',
+  marginBottom:8,
+  color:'#333'
+},
+
+
+input:{
+  backgroundColor:'#fff',
+  borderRadius:10,
+  padding:14,
+  marginBottom:18,
+  borderWidth:1,
+  borderColor:'#ddd',
+  fontSize:15
+},
+
+
+addressInput:{
+  height:100,
+  textAlignVertical:'top'
+},
+
+
+button:{
+  backgroundColor:'#2E7D32',
+  padding:16,
+  borderRadius:12,
+  alignItems:'center',
+  marginTop:10
+},
+
+
+buttonText:{
+  color:'#fff',
+  fontSize:16,
+  fontWeight:'bold'
+}
+
+
+});
