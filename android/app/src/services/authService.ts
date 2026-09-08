d21246207930db6c3bb28033d2e4140ac8e2fcb1
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-08 23:03:45 +0530

    auth service updated

--- android/app/src/services/authService.ts
+++ android/app/src/services/authService.ts
@@ -1,9 +1,6 @@
 import api from './api';
 
-export const login = async (
-  email: string,
-  password: string,
-) => {
+export const login = async (email: string, password: string) => {
   const response = await api.post('/auth/login', {
     email,
     password,
