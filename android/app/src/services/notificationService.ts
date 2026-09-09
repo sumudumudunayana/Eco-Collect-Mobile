8ee8f8cfd35ffea111f9ca78854f1fcc13855bb6
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-09 21:06:54 +0530

    notification service updated

--- android/app/src/services/notificationService.ts
+++ android/app/src/services/notificationService.ts
@@ -15,7 +15,6 @@ export const deleteNotification = async (id: string) => {
   return response.data;
 };
 
-
 export const markNotificationAsRead = async (id: string) => {
   const response = await api.put(`/notifications/${id}/read`);
   return response.data;
