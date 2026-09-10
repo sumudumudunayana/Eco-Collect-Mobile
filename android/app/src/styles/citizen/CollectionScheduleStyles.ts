d79a48071729d763ac3018c5d1ba98748769c963
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-10 12:44:30 +0530

    collection schedule styles updated

--- android/app/src/styles/citizen/CollectionScheduleStyles.ts
+++ android/app/src/styles/citizen/CollectionScheduleStyles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#F8FAFC',
@@ -142,7 +141,6 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: '#64748B',
   },
-
 });
 
 export default styles;
