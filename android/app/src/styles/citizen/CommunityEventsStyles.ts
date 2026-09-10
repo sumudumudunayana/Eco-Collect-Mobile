8fc8276d083d96a4200010ec4a60d63c18a1e3cb
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-10 12:45:11 +0530

    community event styles updated

--- android/app/src/styles/citizen/CommunityEventsStyles.ts
+++ android/app/src/styles/citizen/CommunityEventsStyles.ts
@@ -1,44 +1,36 @@
-import {StyleSheet} from 'react-native';
-
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F9F5',
+  },
 
-container:{
-  flex:1,
-  backgroundColor:'#F5F9F5'
-},
+  eventCard: {
+    backgroundColor: '#fff',
+    margin: 15,
+    padding: 18,
+    borderRadius: 15,
+    elevation: 3,
+  },
 
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#2E7D32',
+    marginBottom: 10,
+  },
 
-eventCard:{
-  backgroundColor:'#fff',
-  margin:15,
-  padding:18,
-  borderRadius:15,
-  elevation:3
-},
+  text: {
+    fontSize: 14,
+    marginBottom: 6,
+    color: '#555',
+  },
 
-
-title:{
-  fontSize:18,
-  fontWeight:'bold',
-  color:'#2E7D32',
-  marginBottom:10
-},
-
-
-text:{
-  fontSize:14,
-  marginBottom:6,
-  color:'#555'
-},
-
-
-description:{
-  marginTop:10,
-  fontSize:14,
-  color:'#444',
-  lineHeight:20
-}
-
-
+  description: {
+    marginTop: 10,
+    fontSize: 14,
+    color: '#444',
+    lineHeight: 20,
+  },
 });
