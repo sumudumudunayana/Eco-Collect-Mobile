69b621e7272942cde9585f622b5bc30e8e90e994
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-10 15:08:50 +0530

    collection details styles updated

--- android/app/src/styles/collector/CollectionDetailsStyles.ts
+++ android/app/src/styles/collector/CollectionDetailsStyles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#F5F7FA',
@@ -80,7 +79,6 @@ const styles = StyleSheet.create({
     fontSize: 17,
     fontWeight: '700',
   },
-
 });
 
 export default styles;
