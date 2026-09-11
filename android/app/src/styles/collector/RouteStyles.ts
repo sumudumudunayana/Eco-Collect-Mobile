054e7d67864df0ebb0a6c8ccb8d6dbc8c31b7ab4
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-11 23:33:41 +0530

    route styles updated

--- android/app/src/styles/collector/RouteStyles.ts
+++ android/app/src/styles/collector/RouteStyles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#F5F7FA',
@@ -103,7 +102,6 @@ const styles = StyleSheet.create({
     fontSize: 17,
     fontWeight: '700',
   },
-
 });
 
 export default styles;
