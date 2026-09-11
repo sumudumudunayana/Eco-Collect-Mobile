a06769bfaf1aad6461c90bd7f478635b828feade
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-11 23:33:13 +0530

    rout card styles updated

--- android/app/src/styles/collector/RouteCardStyles.ts
+++ android/app/src/styles/collector/RouteCardStyles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
   container: {
     backgroundColor: '#FFFFFF',
     borderRadius: 18,
@@ -72,7 +71,6 @@ const styles = StyleSheet.create({
     color: '#2E7D32',
     fontWeight: '700',
   },
-
 });
 
 export default styles;
