d6204a1694e501ea61559f77413826495fc02b8b
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-11 23:32:40 +0530

    daily summery styles updated

--- android/app/src/styles/collector/DailySummaryStyles.ts
+++ android/app/src/styles/collector/DailySummaryStyles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#F5F7FA',
@@ -91,7 +90,6 @@ const styles = StyleSheet.create({
     fontSize: 17,
     fontWeight: '700',
   },
-
 });
 
 export default styles;
