e6588bebd43637b347e7015562f2e40d77e2283c
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-11 23:33:55 +0530

    summeray card styles updated

--- android/app/src/styles/collector/SummaryCardStyles.ts
+++ android/app/src/styles/collector/SummaryCardStyles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     borderRadius: 18,
@@ -38,7 +37,6 @@ const styles = StyleSheet.create({
     color: '#64748B',
     textAlign: 'center',
   },
-
 });
 
 export default styles;
