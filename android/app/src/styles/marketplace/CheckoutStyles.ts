9a2c14ebef7ec4c9fc24ca1722aadd677d8030aa
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-12 14:54:06 +0530

    checkout styles updated

--- android/app/src/styles/marketplace/CheckoutStyles.ts
+++ android/app/src/styles/marketplace/CheckoutStyles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#F5F7FA',
@@ -88,7 +87,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '700',
   },
-
 });
 
 export default styles;
